6c2d212ca31c0590f457fbae7a9c69176a884545
John Emmons <emmons.john@gmail.com>
2017-09-09 11:43:40 -0700

    added run script

---
+++ src/frontend/run1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./my-camera --audio-source alsa_input.usb-Logitech_Logitech_USB_Headset-00.analog-mono --audio-sink alsa_output.usb-Logitech_Logitech_USB_Headset-00.analog-stereo.2 --fps 15 --camera /dev/video0
+
